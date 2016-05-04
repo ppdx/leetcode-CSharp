f1609029cff8d446d03190269f6d68fbd3d6aa19
ppdxxm <ppdxxm@outlook.com>
2016-05-04 16:34:31 +0800

    Implement StrStr() AC

---
+++ leetcode-CSharp/Implement strStr().cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class ImplementStrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0) return 0;
+            if (haystack.Length < needle.Length) return -1;
+            for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                int j = 0;
+                while (j < needle.Length && haystack[i + j] == needle[j])
+                    j++;
+                if (j == needle.Length)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/ImplementStrStrTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class ImplementStrStrTests
+    {
+        [TestMethod()]
+        public void StrStrTest()
+        {
+            Action<string,string> t = (s1, s2) => Assert.AreEqual(s1.IndexOf(s2), new ImplementStrStr().StrStr(s1, s2));
+            t("", "");
+            t("aaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaab");
+            t("abc", "bc");
+        }
+    }
+}
