109d69de6481a6d969fdd8aca98a59bbc67469ee
ppdxxm <ppdxxm@outlook.com>
2016-05-08 20:22:49 +0800

    divide two integers AC

---
+++ leetcode-CSharp/Divide Two Integers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class DivideTwoIntegers
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (divisor == 0) return int.MaxValue;
+            if (divisor == 1) return dividend;
+            if (divisor == -1) return dividend == int.MinValue ? int.MaxValue : -dividend;
+            if (divisor == int.MinValue) return dividend == int.MinValue ? 1 : 0;
+            if (dividend == 0) return 0;
+            int sign = Math.Sign(dividend) == Math.Sign(divisor) ? 1 : -1;
+            int bits = 30;
+            int result = 0;
+            if (dividend == int.MinValue)
+            {
+                result += 1;
+                dividend += Math.Abs(divisor);
+            }
+            divisor = Math.Abs(divisor);
+            dividend = Math.Abs(dividend);
+            while (dividend >= divisor)
+            {
+                for (int i = bits; i >= 0; i--)
+                {
+                    var v = divisor << i;
+                    if ((divisor == v >> i) && dividend >= v)
+                    {
+                        result += 1 << i;
+                        dividend -= v;
+                        bits = i - 1;
+                        break;
+                    }
+                }
+            }
+            return sign == -1 ? -result : result;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/DivideTwoIntegersTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class DivideTwoIntegersTests
+    {
+        [TestMethod()]
+        public void DivideTest()
+        {
+            t(1234, 234);
+            t(-2147483648, 2);
+        }
+
+        void t(int a, int b)
+        {
+            try
+            {
+                Assert.AreEqual(a / b, new DivideTwoIntegers().Divide(a, b));
+            }
+            catch (Exception)
+            {
+                Assert.AreEqual(int.MaxValue, new DivideTwoIntegers().Divide(a, b));
+            }
+        }
+    }
+}
