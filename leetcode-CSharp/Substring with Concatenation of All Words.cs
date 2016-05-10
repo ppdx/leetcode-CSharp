87ae4aacf4e93254835678a9b3b69f2af2645066
ppdxxm <ppdxxm@outlook.com>
2016-05-10 21:11:48 +0800

    substring with concatenation of all words AC

---
+++ leetcode-CSharp/Substring with Concatenation of All Words.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class SubstringWithConcatenationOfAllWords
+    {
+        public IList<int> FindSubstring(string s, string[] words)
+        {
+            if (words.Length == 0) return new int[] { };
+            var all = MakeDict(words);
+            var length = words[0].Length;
+            var count = words.Length;
+            if (s.Length < length * words.Length) return new int[] { };
+            var result = new List<int>(s.Length);
+            for (int i = 0; i < length; i++)
+            {
+                var wordsArray = WordsArray(s, i, length);
+                for (int j = 0; j <= wordsArray.Count - count; j++)
+                {
+                    var round = new Dictionary<string, int>(all);
+                    for (int k = 0; k < count; k++)
+                    {
+                        if (!RemoveIfpossible(round, wordsArray[j + k]))
+                            goto fail;
+                    }
+                    result.Add(i + j * length);
+                    fail:;
+                }
+            }
+            return result;
+        }
+
+        bool RemoveIfpossible(Dictionary<string, int> d, string key)
+        {
+            if (!d.ContainsKey(key))
+                return false;
+            if (d[key] == 1) d.Remove(key);
+            else d[key]--;
+            return true;
+        }
+
+        Dictionary<string, int> MakeDict(string[] words)
+        {
+            var dict = new Dictionary<string, int>(words.Length);
+            foreach (var s in words)
+            {
+                if (dict.ContainsKey(s))
+                    dict[s]++;
+                else
+                    dict[s] = 1;
+            }
+            return dict;
+        }
+
+        IList<string> WordsArray(string s, int offset, int wordLength)
+        {
+            var wordsList = new List<string>();
+            for (; offset <= s.Length - wordLength; offset += wordLength)
+            {
+                wordsList.Add(s.Substring(offset, wordLength));
+            }
+            return wordsList;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/SubstringWithConcatenationOfAllWordsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class SubstringWithConcatenationOfAllWordsTests
+    {
+        [TestMethod()]
+        public void FindSubstringTest()
+        {
+            Action<string, string[], int[]> t = (s, words, r) => Assert.IsTrue(new SubstringWithConcatenationOfAllWords().FindSubstring(s, words).SequenceEqual(r));
+            t("barfoothefoobarman", new[] { "foo", "bar" }, new[] { 0, 9 });
+            t("barfoofoobarthefoobarman", new[] { "bar", "foo", "the" }, new[] { 6, 9, 12 });
+            t("aaa", new[] { "a", "a" }, new[] { 0, 1 });
+            t("wordgoodgoodgoodbestword", new[] { "word", "good", "best", "good" }, new[] { 8 });
+        }
+    }
+}
