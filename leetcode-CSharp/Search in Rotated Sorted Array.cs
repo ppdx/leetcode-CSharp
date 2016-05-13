6fdf9cb6603a923b3bf4cebca00266a2b6bd5390
ppdxxm <ppdxxm@outlook.com>
2016-05-13 21:26:49 +0800

    Search in Rotated Sorted Array AC

---
+++ leetcode-CSharp/Search in Rotated Sorted Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class SearchInRotatedSortedArray
+    {
+        public int Search(int[] nums, int target)
+        {
+            int length = nums.Length;
+            if (length == 0) return -1;
+            if (length == 1) return nums[0] == target ? 0 : -1;
+            return SearchHelper(nums, 0, length - 1, target);
+        }
+
+        private int SearchHelper(int[] nums, int left, int right, int target)
+        {
+            if (left > right) return -1;
+            if (left == right) return nums[left] == target ? left : -1;
+            int center = (left + right) / 2;
+            if (nums[center] == target) return center;
+            if (nums[left] <= nums[center])
+            {
+                if (nums[left] <= target && target < nums[center])
+                    return SearchHelper(nums, left, center - 1, target);
+                return SearchHelper(nums, center + 1, right, target);
+            }
+            else // nums[left] > nums[center]
+            {
+                if (nums[center] < target && target <= nums[right])
+                    return SearchHelper(nums, center + 1, right, target);
+                return SearchHelper(nums, left, center - 1, target);
+            }
+        }
+    }
+}
---
+++ leetcode-CSharpTests/SearchInRotatedSortedArrayTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class SearchInRotatedSortedArrayTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Action<int[], int> t = (nums, target) =>
+                Assert.AreEqual(nums.ToList().IndexOf(target),
+                                new SearchInRotatedSortedArray().Search(nums, target));
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 4);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 5);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 6);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 7);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 1);
+            t(new[] { 4, 5, 6, 7, 0, 1, 2 }, 2);
+            t(new[] { 1 }, 0);
+            t(new[] { 5, 1, 3 }, 5);
+            t(new[] { 3, 1 }, 1);
+        }
+    }
+}
