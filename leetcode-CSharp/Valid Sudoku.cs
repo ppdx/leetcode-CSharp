be363452a4583604e3fb47c0642f224800ec7c08
ppdxxm <ppdxxm@outlook.com>
2016-05-14 12:16:36 +0800

    valid sudoku AC

---
+++ leetcode-CSharp/Valid Sudoku.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[,] board)
+        {
+            bool[,] rows = new bool[9, 9];
+            bool[,] columns = new bool[9, 9];
+            bool[,] cells = new bool[9, 9];
+            for (int row = 0; row < board.GetLength(0); row++)
+                for (int column = 0; column < board.GetLength(1); column++)
+                {
+                    if (board[row, column] == '.') continue;
+                    int n = board[row, column] - '1';
+                    int cell = (row / 3) * 3 + column / 3;
+                    if (rows[row, n] || columns[column, n] || cells[cell, n])
+                        return false;
+                    rows[row, n] = columns[column, n] = cells[cell, n] = true;
+                }
+            return true;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/ValidSudokuTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class ValidSudokuTests
+    {
+        [TestMethod()]
+        public void IsValidSudokuTest()
+        {
+            Action<string, bool> t = (s, b) => Assert.AreEqual(b, new ValidSudoku().IsValidSudoku(SudokuBuilder(s)));
+            t("53..7.... 6..195... .98....6. 8...6...3 4..8.3..1 7...2...6 .6....28. ...419..5 ....8..79", true);
+
+        }
+
+        private char[,] SudokuBuilder(string s)
+        {
+            var rows = s.Split(' ', '\n', '\t');
+            Assert.AreEqual(9, rows.Length);
+            var table = new char[9, 9];
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    table[i, j] = rows[i][j];
+            return table;
+        }
+    }
+}
