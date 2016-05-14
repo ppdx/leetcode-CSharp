0ae1bc5d3f5968f580361ef400fbc8409efc17e9
ppdxxm <ppdxxm@outlook.com>
2016-05-14 10:46:23 +0800

    search insert position AC

---
+++ leetcode-CSharp/Search Insert Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int length = nums.Length;
+            if (length == 0) return 0;
+            if (length == 1) return nums[0] >= target ? 0 : 1;
+            int left = 0, right = length - 1;
+            while (left <= right)
+            {
+                int center = (left + right) / 2;
+                if (nums[center] == target) return center;
+                if (nums[center] < target) left = center + 1;
+                else right = center - 1;
+            }
+            return left;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/SearchInsertPositionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class SearchInsertPositionTests
+    {
+        [TestMethod()]
+        public void SearchInsertTest()
+        {
+            Action<int[], int> t = (nums, target) =>
+             {
+                 int tmp = Array.FindIndex(nums, (i) => i >= target);
+                 Assert.AreEqual(tmp < 0 ? nums.Length : tmp,
+                     new SearchInsertPosition().SearchInsert(nums, target));
+             };
+            t(new[] { 1, 3, 5, 6 }, 5);
+            t(new[] { 1, 3, 5, 6 }, 2);
+            t(new[] { 1, 3, 5, 6 }, 7);
+            t(new[] { 1, 3, 5, 6 }, 0);
+        }
+    }
+}
