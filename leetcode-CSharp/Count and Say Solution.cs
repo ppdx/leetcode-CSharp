15686b0115fa20abe7dc0591c8862286a8f22929
ppdxxm <ppdxxm@outlook.com>
2016-05-14 21:57:46 +0800

    count and say AC

---
+++ leetcode-CSharp/Count and Say Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class CountAndSaySolution
+    {
+        public string CountAndSay(int n)
+        {
+            var state = "1";
+            while (--n > 0)
+            {
+                var next = new StringBuilder(state.Length * 2);
+                for (int i = 0; i < state.Length; i++)
+                {
+                    int count = 1;
+                    while (i < state.Length - 1 && state[i] == state[i + 1])
+                    {
+                        count++;
+                        i++;
+                    }
+                    next.Append(count);
+                    next.Append(state[i]);
+                }
+                state = next.ToString();
+            }
+            return state;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/CountAndSaySolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class CountAndSaySolutionTests
+    {
+        [TestMethod()]
+        public void CountAndSayTest()
+        {
+            var result = new[] { "1", "11", "21", "1211", "111221" };
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(result[i], new CountAndSaySolution().CountAndSay(i + 1));
+            }
+        }
+    }
+}
