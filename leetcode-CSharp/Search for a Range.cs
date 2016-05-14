8da178a25193d149277e39b6bbcbcc991f303c4b
ppdxxm <ppdxxm@outlook.com>
2016-05-14 10:29:22 +0800

    search for a range AC

---
+++ leetcode-CSharp/Search for a Range.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class SearchForARange
+    {
+        private int[] nums;
+        private int target;
+        public int[] SearchRange(int[] nums, int target)
+        {
+            this.nums = nums;
+            this.target = target;
+            if (Array.BinarySearch(nums, target) < 0)
+                return new[] { -1, -1 };
+            return new[] { LowerBound(0, nums.Length - 1), UpperBound(0, nums.Length - 1) };
+        }
+
+        private int LowerBound(int left, int right)
+        {
+            if (left > right) return left;
+            int center = (left + right) / 2;
+            if (nums[center] < target)
+                return LowerBound(center + 1, right);
+            return LowerBound(left, center - 1);
+        }
+
+        private int UpperBound(int left, int right)
+        {
+            if (left > right) return right;
+            int center = (left + right) / 2;
+            if (nums[center] > target)
+                return UpperBound(left, center - 1);
+            return UpperBound(center + 1, right);
+        }
+    }
+}
---
+++ leetcode-CSharpTests/SearchForARangeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class SearchForARangeTests
+    {
+        [TestMethod()]
+        public void SearchRangeTest()
+        {
+            Action<int[], int> t = (nums, target) =>
+             {
+                 int[] r = new SearchForARange().SearchRange(nums, target);
+                 Assert.AreEqual(Array.IndexOf(nums, target), r[0]);
+                 Assert.AreEqual(Array.LastIndexOf(nums, target), r[1]);
+             };
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 8);
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 5);
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 8);
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 8);
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 0);
+            t(new[] { 5, 7, 7, 8, 8, 10 }, 10);
+            t(new[] { 2, 2 }, 3);
+            t(new[] { 1, 2, 3 }, 2);
+
+        }
+    }
+}
