10c3844f0e403c68eaa3bcf1f2614eaf57bd030e
ppdxxm <ppdxxm@outlook.com>
2016-05-31 16:39:56 +0800

    first missing positive AC

---
+++ leetcode-CSharp/First Missing Positive Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class FirstMissingPositiveSolution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            int len = nums.Length;
+            if (len == 0) return 1;
+            bool hasLen = false;
+            for (int i = 0; i < len; i++)
+            {
+                if (nums[i] == len) hasLen = true;
+                if (nums[i] <= 0 || nums[i] >= len) nums[i] = 0;
+            }
+
+            const int mask = 0x40000000;
+            const int unmask = 0x3fffffff;
+            foreach (int n in nums)
+                nums[n & unmask] |= mask;
+
+            for (int i = 1; i < len; i++)
+                if ((nums[i] & mask) == 0) return i;
+            return hasLen ? len + 1 : len;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/FirstMissingPositiveSolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class FirstMissingPositiveSolutionTests
+    {
+        [TestMethod()]
+        public void FirstMissingPositiveTest()
+        {
+            Action<int[], int> t =
+                (a, r) => Assert.AreEqual(r, new FirstMissingPositiveSolution().FirstMissingPositive(a));
+            t(new[] { 1, 2, 0 }, 3);
+            t(new[] { 3, 4, -1, 1 }, 2);
+            t(new[] { 1 }, 2);
+        }
+    }
+}
