258323e0cfa1359714c7499f6994f82926708237
ppdxxm <ppdxxm@outlook.com>
2016-06-08 20:15:31 +0800

    multipy strings AC

---
+++ leetcode-CSharp/Multiply Strings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class MultiplyStrings
+    {
+        public string Multiply(string num1, string num2)
+        {
+            if (string.IsNullOrWhiteSpace(num1) || string.IsNullOrWhiteSpace(num2)
+                || num1 == "0" || num2 == "0")
+                return "0";
+            var a = num1.Select(c => c - '0').Reverse().ToList();
+            var b = num2.Select(c => c - '0').Reverse().ToList();
+            var result = new int[a.Count + b.Count + 10];
+
+            for (int i = 0; i < a.Count; i++)
+                for (int j = 0; j < b.Count; j++)
+                {
+                    int x = i + j;
+                    result[x] +=
+                        a[i] *
+                        b[j];
+                    while (result[x] > 9)
+                    {
+                        result[x + 1] += result[x] / 10;
+                        result[x] %= 10;
+                        x++;
+                    }
+                }
+            return string.Join("",
+                result.Select(i => (char)(i + '0')).Reverse().SkipWhile(c => c == '0'));
+        }
+    }
+}
---
+++ leetcode-CSharpTests/Multiply StringsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class MultiplyStringsTests
+    {
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Action<string, string> t = (a, b) =>
+                Assert.AreEqual((BigInteger.Parse(a) * BigInteger.Parse(b)).ToString(),
+                                 new MultiplyStrings().Multiply(a, b));
+            t("0", "0");
+            t("6632134512315464321346546879631316832", "864123727643923187694564398798756398456394856043985270");
+        }
+    }
+}
