39b72265ac71d5c984c1752d5266c6f245317483
ppdxxm <ppdxxm@outlook.com>
2016-06-19 15:04:15 +0800

    jump game II AC

---
+++ leetcode-CSharp/Jump Game II.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            int len = nums.Length;
+            if (len <= 1) return 0;
+            var jumps = Enumerable.Repeat(int.MaxValue, len).ToArray();
+            jumps[0] = 0;
+            for (int i = 0; i < len - 1; i++)
+            {
+                int max = Math.Min(i + nums[i] + 1, len);
+                int jump = jumps[i] + 1;
+                for (int j = i; j < max; j++)
+                {
+                    if (jump < jumps[j])
+                        jumps[j] = jump;
+                }
+            }
+            return jumps.Last();
+        }
+    }
+}
---
+++ leetcode-CSharpTests/JumpGameIITests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class JumpGameIITests
+    {
+        [TestMethod()]
+        public void JumpTest()
+        {
+            T(new[] { 2, 3, 1, 1, 4 }, 2);
+        }
+
+        void T(int[] a1, int r) =>
+            Assert.AreEqual(r, new JumpGameII().Jump(a1));
+    }
+}
