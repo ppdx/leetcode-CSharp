d3db76f3f69db9fffeffc2f46dcd641f5b808126
ppdxxm <ppdxxm@outlook.com>
2016-06-20 14:50:29 +0800

    Permutations II AC

---
+++ leetcode-CSharp/Permutations II.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class PermutationsII
+    {
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            Array.Sort(nums);
+            int len = Enumerable.Range(1, nums.Length).Aggregate((a, b) => a * b);
+            var res = new List<IList<int>>(len);
+            for (;;)
+            {
+                res.Add((int[])nums.Clone());
+                if (!NextPermutation(nums)) break;
+            }
+            return res;
+
+        }
+
+        bool NextPermutation(int[] nums)
+        {
+            if (nums.Length < 2)
+            {
+                return false;
+            }
+            int i = nums.Length - 2;
+            for (; i >= 0 && nums[i] >= nums[i + 1]; i--)
+                ;
+            if (i == -1)
+            {
+                Reverse(nums, 0);
+                return false;
+            }
+            else
+            {
+                int j = nums.Length;
+                while (nums[i] >= nums[--j]) ;
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                Reverse(nums, i + 1);
+                return true;
+            }
+        }
+
+        void Reverse(int[] nums, int i)
+        {
+            int j = nums.Length - 1;
+            while (i < j)
+            {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                i++; j--;
+            }
+        }
+
+    }
+}
---
+++ leetcode-CSharpTests/PermutationsIITests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class PermutationsIITests
+    {
+        [TestMethod()]
+        public void PermuteUniqueTest()
+        {
+            T(new[] { 1, 1, 2 }, new[] {  new[]{1,1,2},
+  new[]{1,2,1},
+  new[]{2,1,1}});
+        }
+
+        void T(int[] a, IList<IList<int>> r)
+        {
+            r.AreSetEqual(new PermutationsII().PermuteUnique(a));
+        }
+
+    }
+}
