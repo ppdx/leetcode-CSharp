b342385dc1f4ca5f0d800879d6cf70865b355d7d
ppdxxm <ppdxxm@outlook.com>
2016-07-05 21:16:03 +0800

    Pow(x, n) AC

---
+++ leetcode-CSharp/Pow(x, n).cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class PowXN
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return x;
+            if (n == -1) return 1 / x;
+            double half = MyPow(x, n / 2);
+            if (n % 2 == 0) return half * half;
+            return half * half * (n > 0 ? x : 1 / x);
+        }
+    }
+}
---
+++ leetcode-CSharpTests/PowXNTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class PowXNTests
+    {
+        [TestMethod()]
+        public void MyPowTest()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                double x = rand.NextDouble();
+                int n = rand.Next();
+                Assert.AreEqual(Math.Pow(x, n), new PowXN().MyPow(x, n));
+            }
+            Assert.AreEqual(1, new PowXN().MyPow(1.00000, -2147483648));
+        }
+    }
+}
