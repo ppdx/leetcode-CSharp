f9f7d77e54bfd3305db729bab37ef7b2953e2368
ppdxxm <ppdxxm@outlook.com>
2017-01-09 21:22:20 +0800

    Maximum Subarray AC

---
+++ leetcode-CSharp/Maximum Subarray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class MaximumSubarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSum = int.MinValue;
+            int currentSum = 0;
+            foreach (int n in nums)
+            {
+                currentSum += n;
+                if (maxSum < currentSum) maxSum = currentSum;
+                if (currentSum < 0) currentSum = 0;
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/MaximumSubarrayTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class MaximumSubarrayTests
+    {
+        [TestMethod()]
+        public void MaxSubArrayTest()
+        {
+            Assert.AreEqual(6, new MaximumSubarray().MaxSubArray(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+    }
+}
