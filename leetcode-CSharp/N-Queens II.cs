bfb83f535b79677b584edf4228b7a29e0f8d6975
ppdxxm <ppdxxm@outlook.com>
2017-01-09 20:07:47 +0800

    N-Queens II AC

---
+++ leetcode-CSharp/N-Queens II.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class N_Queens_II
+    {
+        public int TotalNQueens(int n)
+        {
+            if (n == 1) return 1;
+            int count = 0;
+            int[] board = Enumerable.Repeat(-1, n).ToArray();
+            board[0] = 0;
+            int row = 1;
+            while (board[0] != -1)
+            {
+                board[row]++;
+                for (; board[row] < n; board[row]++)
+                {
+                    if (CheckBoard(board, row))
+                    {
+                        row++;
+                        break;
+                    }
+                }
+                if (row == n)
+                {
+                    row--;
+                    count++;
+                }
+                else if (board[row] == n)
+                {
+                    board[row] = -1;
+                    row--;
+                }
+
+            }
+            return count;
+        }
+
+        private bool CheckBoard(int[] board, int row)
+        {
+            int n = board.Length;
+            int col = board[row];
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i] == j) return false;
+            }
+            for (int i = row - 1; i >= 0; i--)
+            {
+                if (board[i] == col) return false;
+            }
+            for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            {
+                if (board[i] == j) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/N_Queens_IITests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class N_Queens_IITests
+    {
+        [TestMethod()]
+        public void TotalNQueensTest()
+        {
+            Assert.AreEqual(1, new N_Queens_II().TotalNQueens(1), "arg: 1");
+            Assert.AreEqual(2, new N_Queens_II().TotalNQueens(4), "arg: 4");
+        }
+    }
+}
