ac0a6408508fee6173f25fed932a55a0560a7529
ppdxxm <ppdxxm@outlook.com>
2017-01-09 19:33:23 +0800

    N-Queens AC

---
+++ leetcode-CSharp/N-Queens.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class NQueens
+    {
+        private string[] lines;
+        public IList<IList<string>> SolveNQueens(int n)
+        {
+            if (n == 1) return new List<IList<string>> { new[] { "Q" } };
+            lines = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                lines[i] = new StringBuilder(n).Append('.', i).Append('Q').Append('.', n - i - 1).ToString();
+            }
+            List<IList<string>> result = new List<IList<string>>();
+            int[] board = Enumerable.Repeat(-1, n).ToArray();
+            board[0] = 0;
+            int row = 1;
+            while (board[0] != -1)
+            {
+                board[row]++;
+                for (; board[row] < n; board[row]++)
+                {
+                    if (CheckBoard(board, row))
+                    {
+                        row++;
+                        break;
+                    }
+                }
+                if (row == n)
+                {
+                    row--;
+                    result.Add(ToStringArray(board));
+                }
+                else if (board[row] == n)
+                {
+                    board[row] = -1;
+                    row--;
+                }
+
+            }
+            return result;
+        }
+
+        private IList<string> ToStringArray(int[] board)
+        {
+            return board.Select(i => lines[i]).ToArray();
+        }
+
+        private bool CheckBoard(int[] board, int row)
+        {
+            int n = board.Length;
+            int col = board[row];
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i] == j) return false;
+            }
+            for (int i = row - 1; i >= 0; i--)
+            {
+                if (board[i] == col) return false;
+            }
+            for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            {
+                if (board[i] == j) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/NQueensTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetcode_CSharp.Tests;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class NQueensTests
+    {
+        [TestMethod()]
+        public void SolveNQueensTest()
+        {
+            var result = new NQueens().SolveNQueens(4);
+            var resultSet = new HashSet<string>();
+            foreach (var item in result)
+            {
+                resultSet.Add(string.Join(" ", item));
+            }
+            var answer = new HashSet<string> { ".Q.. ...Q Q... ..Q.", "..Q. Q... ...Q .Q.." };
+            Assert.IsTrue(resultSet.SetEquals(answer));
+        }
+    }
+}
