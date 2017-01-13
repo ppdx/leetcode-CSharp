e5c52617697751f752fb41ced094591e4e7b980d
ppdxxm <ppdxxm@outlook.com>
2017-01-13 20:58:46 +0800

    Spiral Matrix AC

---
+++ leetcode-CSharp/Spiral Matrix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class SpiralMatrix
+    {
+        public IList<int> SpiralOrder(int[,] matrix)
+        {
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            var result = new List<int>(row * col);
+            int x = 0, y = 0;
+            int delteX = 0, delteY = 1;
+            int count = 0;
+            while (count < row * col)
+            {
+                result.Add(matrix[x, y]);
+                if (delteY == 1 && y == col - x-1)
+                {
+                    delteX = 1;
+                    delteY = 0;
+                }
+                if (delteX == 1 && x == row - col + y)
+                {
+                    delteX = 0;
+                    delteY = -1;
+                }
+                if (delteY == -1 && y == row - x-1)
+                {
+                    delteX = -1;
+                    delteY = 0;
+                }
+                if (delteX == -1 && x == y + 1)
+                {
+                    delteX = 0;
+                    delteY = 1;
+                }
+                x += delteX;
+                y += delteY;
+                count++;
+            }
+            return result;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/SpiralMatrixTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class SpiralMatrixTests
+    {
+        [TestMethod()]
+        public void SpiralOrderTest()
+        {
+            new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 }.AreSequenceEqual(
+                new SpiralMatrix().SpiralOrder(new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
+            new[] { 1, 2 }.AreSequenceEqual(new SpiralMatrix().SpiralOrder(new int[,] { { 1 }, { 2 } }));
+        }
+    }
+}
