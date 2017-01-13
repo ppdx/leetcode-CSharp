4bb8a627b6604808ce368d48dd334a6fa18622d7
ppdxxm <ppdxxm@outlook.com>
2017-01-13 21:45:16 +0800

    Jump Game AC

---
+++ leetcode-CSharp/Jump Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp
+{
+    public class Jump_Game
+    {
+        public bool CanJump(int[] nums)
+        {
+            int n = nums.Length;
+            if (n < 2) return true;
+            bool[] positions = new bool[n];
+            positions[0] = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (!positions[i]) return false;
+                if (i + nums[i] >= n) return true;
+                for (int j = 1; j <= nums[i]; j++)
+                {
+                    positions[i + j] = true;
+                    if (nums[i] <= nums[i + j] + j) break;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcode-CSharpTests/Jump_GameTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcode_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_CSharp.Tests
+{
+    [TestClass()]
+    public class Jump_GameTests
+    {
+        [TestMethod()]
+        public void CanJumpTest()
+        {
+            Assert.IsTrue(new Jump_Game().CanJump(new[] { 2, 3, 1, 1, 4 }));
+            Assert.IsFalse(new Jump_Game().CanJump(new[] { 3, 2, 1, 0, 4 }));
+        }
+    }
+}
